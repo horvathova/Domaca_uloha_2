b99b611beaf2de40a93fdf11194f1510ae90c45b
horvathova <nikoleta638@gmail.com>
2017-04-25 01:56:52 +0200

    Delete 28_3_2017_men-suboru.c

--- 28_3_2017_men-suboru.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<string.h>
-#define __FILENAME__ (strrchr(__FILE__, '\\') ? strrchr(__FILE__, '\\') + 1 : __FILE__)
-
-int main(int argc, char** argv) {
-
-    printf("%s",  __FILENAME__);
-
-}
